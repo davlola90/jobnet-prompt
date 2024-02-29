8144ffb417414d86fb52acf7774cedaaab987978
Aymen Toukabri <aymen@jobnet.se>
2024-02-29 16:54:58 +0100

    add open api

--- JobnetPrompt/obj/Debug/net6.0/JobnetPrompt.AssemblyInfo.cs
+++ JobnetPrompt/obj/Debug/net6.0/JobnetPrompt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JobnetPrompt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ba2d360ccc9625ecb31288a476678dfe0a11a80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("JobnetPrompt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JobnetPrompt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
